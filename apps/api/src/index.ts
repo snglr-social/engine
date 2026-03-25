13e2dfb18dfe6e04e2609dafdfc86187221af166
クロナミ Abyss <joibelitz374@hotmail.com>
2026-03-25 03:08:10 +0300

    ✨ (api): Add chat routes with Prisma

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,26 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+import { Chats } from "./features/chats/list-chats";
+import { CreateChat } from "./features/chats/create-chat";
+import { CreateChatSchema } from "./features/chats/create-chat/schema";
+
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+const db = new PrismaClient({ adapter });
+
+const chats = new Chats(db);
+const createChat = new CreateChat(db);
+
+const app = new Elysia()
+	.get("/chats", chats.handler)
+	.post("/chats", createChat.handler, {
+		body: CreateChatSchema,
+	})
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`😜 snglr-social/engine is running at ${app.server?.hostname}:${app.server?.port}`,
 );
